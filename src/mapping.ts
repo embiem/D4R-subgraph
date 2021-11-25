5b2d44d1be52019fec73a3350d2dd95d979ac9f0
Martin Beierling-Mutz <mbeierling@gmail.com>
2021-11-25 01:49:49 +0100

    initial functionality

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,76 +1,44 @@
-import { BigInt } from "@graphprotocol/graph-ts"
-import {
-  DEVS,
-  Approval,
-  ApprovalForAll,
-  OwnershipTransferred,
-  Transfer
-} from "../generated/DEVS/DEVS"
-import { ExampleEntity } from "../generated/schema"
+import { DEVS, Transfer } from "../generated/DEVS/DEVS";
+import { Account, Token } from "../generated/schema";
 
-export function handleApproval(event: Approval): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+export function handleTransfer(event: Transfer): void {
+  const { tokenId, from, to } = event.params;
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+  const contract = DEVS.bind(event.address);
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+  let fromAccount = Account.load(from.toHex());
+  let toAccount = Account.load(to.toHex());
+
+  if (!toAccount) {
+    toAccount = new Account(to.toHex());
   }
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+  // Just create new, as we don't need to use values from store
+  const token = new Token(tokenId.toString());
+  token.owner = to.toHex();
+  token.lastTransferTime = event.block.timestamp;
+  token.lastTransferBlock = event.block.number;
 
-  // Entity fields can be set based on event parameters
-  entity.owner = event.params.owner
-  entity.approved = event.params.approved
+  token.clothing = contract.getClothing(tokenId);
+  token.industry = contract.getIndustry(tokenId);
+  token.language = contract.getLanguage(tokenId);
+  token.location = contract.getLocation(tokenId);
+  token.mind = contract.getMind(tokenId);
+  token.os = contract.getOS(tokenId);
+  token.texteditor = contract.getTextEditor(tokenId);
+  token.vibe = contract.getVibe(tokenId);
 
-  // Entities can be written to the store with `.save()`
-  entity.save()
+  token.save();
 
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
+  // Reference in account
+  toAccount.tokens.push(tokenId.toString());
+  toAccount.save();
 
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.balanceOf(...)
-  // - contract.getApproved(...)
-  // - contract.getClothing(...)
-  // - contract.getIndustry(...)
-  // - contract.getLanguage(...)
-  // - contract.getLocation(...)
-  // - contract.getMind(...)
-  // - contract.getOS(...)
-  // - contract.getTextEditor(...)
-  // - contract.getVibe(...)
-  // - contract.isApprovedForAll(...)
-  // - contract.name(...)
-  // - contract.owner(...)
-  // - contract.ownerOf(...)
-  // - contract.supportsInterface(...)
-  // - contract.symbol(...)
-  // - contract.tokenByIndex(...)
-  // - contract.tokenOfOwnerByIndex(...)
-  // - contract.tokenURI(...)
-  // - contract.totalSupply(...)
+  // Remove from sender's account, if applicable
+  if (fromAccount) {
+    fromAccount.tokens = fromAccount.tokens.filter(
+      (token) => token !== tokenId.toString()
+    );
+    fromAccount.save();
+  }
 }
-
-export function handleApprovalForAll(event: ApprovalForAll): void {}
-
-export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
-
-export function handleTransfer(event: Transfer): void {}
